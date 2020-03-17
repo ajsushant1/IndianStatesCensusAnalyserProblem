75fb9d1aa38322b536657ccfade8c0264242bd3d
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-17 09:52:38 +0530

    ADDED: Welcome message

---
+++ src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.statecensusanalysertest;
+
+import com.bridgelabz.statecensusanalyser.StateCensusAnalyser;
+
+public class StateCensusAnalyserTest {
+    StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+}
