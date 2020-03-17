d68722a6f4d5cb6c0a71ec4d7661271cf6d6600b
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-17 11:50:07 +0530

    ADDED: Test method to check to ensure number of record matches

--- src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
@@ -1,7 +1,22 @@
 package com.bridgelabz.statecensusanalysertest;
 
 import com.bridgelabz.statecensusanalyser.StateCensusAnalyser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
 
 public class StateCensusAnalyserTest {
     StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+    private static final String CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
+
+    @Test
+    public void givenTheStatesCensusCSVFile_WhenNumberOfRecordMatches_ShouldReturnTrue() {
+        try {
+            int numberOfRecords = stateCensusAnalyser.loadCSVData(CSV_FILE_PATH);
+            Assert.assertEquals(29, numberOfRecords);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
