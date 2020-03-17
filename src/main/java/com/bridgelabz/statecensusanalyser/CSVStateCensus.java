25ec5cf39b46620808f03ac651a09ec538d6fad2
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-17 12:04:41 +0530

    ADDED: POJO class to read CSV file directly

--- src/main/java/com/bridgelabz/statecensusanalyser/CSVStateCensus.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/CSVStateCensus.java
@@ -1,4 +1,14 @@
 package com.bridgelabz.statecensusanalyser;
 
+import com.opencsv.bean.CsvBindByName;
+
 public class CSVStateCensus {
+    @CsvBindByName(column = "State")
+    private String state;
+    @CsvBindByName(column = "Population")
+    private int population;
+    @CsvBindByName(column = "AreaInSqKm")
+    private int area;
+    @CsvBindByName(column = "DensityPerSqKm")
+    private int density;
 }
