92c9d8493d563e40cd7146d522fb82aed2522891
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-18 10:28:46 +0530

    ADDED: Test case to throw custom exception for file type

--- src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
@@ -5,12 +5,11 @@ import com.bridgelabz.statecensusanalyserexception.StateCensusAnalyserException;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.IOException;
-
 public class StateCensusAnalyserTest {
     StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
     private static final String CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
-    private static final String FILE_PATH = "src/test/resource/StateCensusData.csv";
+    private static final String INCORRECT_FILE_NAME = "src/test/resource/StateCensusData.csv";
+    private static final String INCORRECT_FILE_TYPE = "src/test/resources/StateCensusData.pdf";
 
     @Test
     public void givenTheStatesCensusCSVFile_WhenNumberOfRecordMatches_ShouldReturnTrue() {
@@ -19,19 +18,24 @@ public class StateCensusAnalyserTest {
             Assert.assertEquals(29, numberOfRecords);
         } catch (StateCensusAnalyserException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
     @Test
     public void givenTheStateCensusCSVFile_WhenIncorrect_ShouldThrowCustomException() {
         try {
-            stateCensusAnalyser.loadCSVData(FILE_PATH);
+            stateCensusAnalyser.loadCSVData(INCORRECT_FILE_NAME);
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE, e.type);
-        } catch (IOException e) {
-            e.getStackTrace();
+        }
+    }
+
+    @Test
+    public void givenTheStateCensusCSVFile_WhenIncorrectFileType_ShouldThrowCustomException() {
+        try {
+            stateCensusAnalyser.loadCSVData(INCORRECT_FILE_TYPE);
+        } catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INCORRECT_FILE_TYPE, e.type);
         }
     }
 }
