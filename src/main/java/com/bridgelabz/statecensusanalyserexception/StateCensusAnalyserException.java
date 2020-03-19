d17089276bb23df01be706d7494ad458740c41d8
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-19 10:06:08 +0530

    ADDED: Exception type for incorrect header

--- src/main/java/com/bridgelabz/statecensusanalyserexception/StateCensusAnalyserException.java
+++ src/main/java/com/bridgelabz/statecensusanalyserexception/StateCensusAnalyserException.java
@@ -2,16 +2,16 @@ package com.bridgelabz.statecensusanalyserexception;
 
 public class StateCensusAnalyserException extends Exception {
 
-    //ENUM CLASS
-    public enum ExceptionType {
-        NO_SUCH_FILE, INCORRECT_FILE_TYPE, INCORRECT_DELIMITER;
-    }
-
     public ExceptionType type;
 
     //CONSTRUCTOR
     public StateCensusAnalyserException(ExceptionType type, String message) {
         super(message);
         this.type = type;
+    }
+
+    //ENUM CLASS
+    public enum ExceptionType {
+        NO_SUCH_FILE, INCORRECT_FILE_TYPE, INCORRECT_DELIMITER_OR_HEADER
     }
 }
