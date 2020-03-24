d7d47543ec0e02e4f11ef1440c9295fa5817adce
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-24 23:41:08 +0530

    REFACTORED: Removed getter method

--- src/main/java/com/bridgelabz/statecensusanalyser/CSVStateCode.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/CSVStateCode.java
@@ -4,27 +4,12 @@ import com.opencsv.bean.CsvBindByName;
 
 public class CSVStateCode {
     @CsvBindByName(column = "SrNo", required = true)
-    private int serialNumber;
-    @CsvBindByName(column = "StateName",required = true)
-    private String stateName;
-    @CsvBindByName(column = "TIN",required = true)
-    private int tin;
-    @CsvBindByName(column = "StateCode",required = true)
-    private String stateCode;
+    public int serialNumber;
+    @CsvBindByName(column = "StateName", required = true)
+    public String stateName;
+    @CsvBindByName(column = "TIN", required = true)
+    public int tin;
+    @CsvBindByName(column = "StateCode", required = true)
+    public String stateCode;
 
-    public int getSerialNumber() {
-        return serialNumber;
-    }
-
-    public String getStateName() {
-        return stateName;
-    }
-
-    public int getTin() {
-        return tin;
-    }
-
-    public String getStateCode() {
-        return stateCode;
-    }
 }
