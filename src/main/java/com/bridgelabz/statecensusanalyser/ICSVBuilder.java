8d8de654765926e4a3bb5348b9984717c6a65664
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-24 23:26:47 +0530

    ADDED: Method to access csv data using Iterator

--- src/main/java/com/bridgelabz/statecensusanalyser/ICSVBuilder.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/ICSVBuilder.java
@@ -3,8 +3,11 @@ package com.bridgelabz.statecensusanalyser;
 import com.bridgelabz.statecensusanalyserexception.CSVBuilderException;
 
 import java.io.Reader;
+import java.util.Iterator;
 import java.util.List;
 
 public interface ICSVBuilder {
     <T> List<T> getCSVList(Reader reader, Class<T> csvClass) throws CSVBuilderException;
+
+    <T> Iterator<T> getCSVIterator(Reader reader, Class<T> csvClass) throws CSVBuilderException;
 }
