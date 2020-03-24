3d056f0b8b36a3e69461abfdc098a7abcb62595d
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-24 23:38:20 +0530

    REFACTORED: Code to inherit interface method

--- src/main/java/com/bridgelabz/statecensusanalyser/OpenCSVBuilder.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/OpenCSVBuilder.java
@@ -2,16 +2,16 @@ package com.bridgelabz.statecensusanalyser;
 
 
 import com.bridgelabz.statecensusanalyserexception.CSVBuilderException;
-
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.Reader;
+import java.util.Iterator;
 import java.util.List;
 
 public class OpenCSVBuilder implements ICSVBuilder {
 
-    //METHOD ITERATE CSV DATA FROM FILE
+    //METHOD ITERATE CSV DATA FROM FILE USING LIST
     @Override
     public <T> List<T> getCSVList(Reader reader, Class<T> csvClass) throws CSVBuilderException {
         try {
@@ -23,5 +23,20 @@ public class OpenCSVBuilder implements ICSVBuilder {
         } catch (IllegalStateException e) {
             throw new CSVBuilderException(CSVBuilderException.ExceptionType.UNABLE_TO_PARSE, "Unable to parse");
         }
+    }
+
+    //METHOD ITERATE CSV DATA FROM FILE USING ITERATOR
+    @Override
+    public <T> Iterator<T> getCSVIterator(Reader reader, Class<T> csvClass) throws CSVBuilderException {
+        try {
+            CsvToBean csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(csvClass)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            return csvToBean.iterator();
+        } catch (IllegalStateException e) {
+            throw new CSVBuilderException(CSVBuilderException.ExceptionType.UNABLE_TO_PARSE, "Unable to parse");
+        }
+
     }
 }
