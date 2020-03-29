403f92286e27643e708d9c16993ed4cd481b2c7e
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-30 00:11:58 +0530

    ADDED: DAO for USCensus

--- src/main/java/com/bridgelabz/statecensusanalyser/CensusDAO.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/CensusDAO.java
@@ -13,4 +13,12 @@ public class CensusDAO {
         this.area = csvStateCensus.area;
         this.density = csvStateCensus.density;
     }
+
+    public CensusDAO(CSVUSCensus csvusCensus) {
+        this.state = csvusCensus.state;
+        this.stateCode = csvusCensus.stateId;
+        this.population = csvusCensus.population;
+        this.area = csvusCensus.totalArea;
+        this.density = csvusCensus.populationDensity;
+    }
 }
