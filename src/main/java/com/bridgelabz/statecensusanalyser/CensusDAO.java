38217cabc3b8be1e3f806b55146be510c3d05f83
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-31 02:04:13 +0530

    REFACTORED: Code to return Indian census csv as DTO to ensure encapsulation

--- src/main/java/com/bridgelabz/statecensusanalyser/CensusDAO.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/CensusDAO.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.statecensusanalyser;
 
+import java.util.Comparator;
+
 public class CensusDAO {
     public String state;
     public String stateCode;
@@ -20,5 +22,35 @@ public class CensusDAO {
         this.population = csvusCensus.population;
         this.area = csvusCensus.totalArea;
         this.density = csvusCensus.populationDensity;
+    }
+
+    public static Comparator<CensusDAO> getSortComparator(StateCensusAnalyser.SortingMode mode) {
+        if (mode.equals(StateCensusAnalyser.SortingMode.STATE))
+            return Comparator.comparing(census -> census.state);
+        if (mode.equals(StateCensusAnalyser.SortingMode.POPULATION))
+            return Comparator.comparing(CensusDAO::getPopulation).reversed();
+        if (mode.equals(StateCensusAnalyser.SortingMode.AREA))
+            return Comparator.comparing(CensusDAO::getArea).reversed();
+        if (mode.equals(StateCensusAnalyser.SortingMode.DENSITY))
+            return Comparator.comparing(CensusDAO::getDensity).reversed();
+        return null;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getDensity() {
+        return density;
+    }
+
+    public Object getCensusDTO(StateCensusAnalyser.Country country) {
+        if (country.equals(StateCensusAnalyser.Country.INDIA))
+            return new CSVStateCensus(state, population, area, density);
+        return null;
     }
 }
